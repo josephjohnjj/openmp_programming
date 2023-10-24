000eee3fad43f91cd4093a3e74770881dd11e0e1
Joseph John <joseph.john@anu.edu.au>
2023-10-24 19:11:06 +1100

    depends clause

--- openmp/openmp_depend.c
+++ openmp/openmp_depend.c
@@ -3,17 +3,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+int write_val(int *var, int value)
+{
+	*var = value;
+}
 
+int read_val(int *var)
+{
+	return *var;
+}
 
 int main ( int argc, char *argv[] )
 {
-int x = 1;
-   #pragma omp parallel
-   #pragma omp single
-   {
-       #pragma omp task shared(x) depend(out: x)
-       x = 2;
-       #pragma omp task shared(x) depend(in: x)
-       printf("x = %d\n", x);
+	int x = 10;
+	int y = 20;
+	int z = 0;
+
+   	#pragma omp parallel
+   	#pragma omp single
+   	{
+
+       		#pragma omp task shared(x) depend(out: x)
+       		write_val(&x, 10);
+
+       		#pragma omp task shared(x) depend(out: y)
+       		write_val(&y, 10);
+
+		#pragma omp task shared(x, y) depend(in: x, y)
+		{
+			z = read_val(&x) + read_val(&y);
+			printf("Sum = %d \n", z);
+		}
+	}
+
+	return 0;
 }
-return 0; } 
+
